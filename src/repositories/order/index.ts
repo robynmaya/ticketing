dd5dc80ce61f50100d9c14dab0db64b45cf01178
Robyn <hellothisisrobyn@gmail.com>
2025-06-24 09:07:51 -0400

    fix: force mock createOrder in production build

--- src/repositories/order/index.ts
+++ src/repositories/order/index.ts
@@ -1,5 +1,7 @@
 import { createOrder as mockCreateOrder } from './mock';
 import { createOrder as prodCreateOrder } from './production';
 
-export const createOrder =
-  process.env.NODE_ENV === 'production' ? prodCreateOrder : mockCreateOrder;
+const useMocks = process.env.REACT_APP_USE_MOCKS === 'true';
+export const createOrder = useMocks
+  ? mockCreateOrder
+  : prodCreateOrder;
