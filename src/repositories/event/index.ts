f6ce0fede4fad07b3f9a01a42f2e1c6b449d81b9
Robyn <hellothisisrobyn@gmail.com>
2025-06-24 08:40:19 -0400

    chore: allow toggling mock vs prod via REACT_APP_USE_MOCKS

--- src/repositories/event/index.ts
+++ src/repositories/event/index.ts
@@ -1,6 +1,8 @@
 import { getEventById as mockGetEventById } from "./mock";
 import { getEventById as productionGetEventById } from "./production";
 
-export const getEventById = process.env.NODE_ENV === 'production' 
+const useMocks = process.env.REACT_APP_USE_MOCKS === 'true';
+
+export const getEventById = useMocks
     ? productionGetEventById 
     : mockGetEventById
